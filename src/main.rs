675bbd491d426fc182e5837e01a3e2985183e41f
Juan Milkah <juanmilkah@gmail.com>
2025-01-28 16:59:42 +0300

    add lexer

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 use std::collections::HashMap;
+use std::env;
 use std::fs::{self, File};
-use std::{
-    env,
-    io::{self, BufReader, BufWriter},
-};
+use std::io::{self, BufReader, BufWriter};
 
 use lopdf::Document;
 
 enum Commands {
-    Search { term: String, index_file: String },
+    Search { index_file: String, term: String },
     Index { files_dir: String },
 }
 
@@ -19,14 +17,15 @@ struct VectorCompare;
 
 impl VectorCompare {
     // count of every word that occurs in a document
-    fn concodance(&self, document: String) -> HashMap<String, f32> {
-        let mut map = HashMap::new();
+    fn concodance(&self, tokens: &[String], mut map: DocIndex) -> DocIndex {
+        // let tokens: Vec<String> = Lexer::new().parse(document);
 
-        for word in document.split(" ") {
-            let mut count: f32 = *map.entry(word.to_string()).or_insert(0.0);
+        for token in tokens {
+            let token = token.trim_ascii();
+            let mut count: f32 = *map.entry(token.to_string()).or_insert(0.0);
             count += 1.0;
 
-            map.insert(word.to_string(), count);
+            map.insert(token.to_string(), count);
         }
 
         map
@@ -62,13 +61,57 @@ impl VectorCompare {
     }
 }
 
+struct Lexer {
+    input: String,
+    position: usize,
+}
+
+impl Lexer {
+    fn new(input: &str) -> Self {
+        Self {
+            input: input.to_lowercase(),
+            position: 0,
+        }
+    }
+
+    fn parse(&mut self) -> Vec<String> {
+        let mut tokens = Vec::new();
+        let chars: Vec<char> = self.input.chars().collect();
+
+        while self.position < chars.len() {
+            let current = chars[self.position];
+            let start = self.position;
+
+            if current == '\n' || !current.is_ascii_alphanumeric() {
+                self.position += 1;
+                continue;
+            }
+
+            if current.is_ascii_alphanumeric() {
+                while self.position < chars.len() && chars[self.position].is_ascii_alphanumeric() {
+                    self.position += 1;
+                }
+            } else {
+                self.position += 1;
+            }
+
+            let token: String = chars[start..self.position].iter().collect();
+
+            if !token.is_empty() {
+                tokens.push(token);
+            }
+        }
+
+        tokens
+    }
+}
+
 fn main() -> io::Result<()> {
     let v = VectorCompare;
     match entry() {
         Ok(val) => match val {
             Commands::Index { files_dir } => {
-                let docs = fs::read_dir(&files_dir)
-                    .unwrap()
+                let docs = fs::read_dir(&files_dir)?
                     .map(|entry| entry.unwrap().path().to_string_lossy().to_string())
                     .collect::<Vec<String>>();
 
@@ -79,7 +122,7 @@ fn main() -> io::Result<()> {
                     ));
                 }
             }
-            Commands::Search { term, index_file } => {
+            Commands::Search { index_file, term } => {
                 let term_matches = match search_term(&v, &term, &index_file) {
                     Ok(val) => val,
                     Err(err) => {
@@ -105,7 +148,7 @@ fn main() -> io::Result<()> {
 
 fn index_documents(v: &VectorCompare, docs: &[String]) -> io::Result<()> {
     //build the index
-    let mut index = HashMap::new();
+    let mut docs_index = HashMap::new();
 
     for doc in docs.iter() {
         println!("Indexing document: {doc}");
@@ -116,23 +159,31 @@ fn index_documents(v: &VectorCompare, docs: &[String]) -> io::Result<()> {
                 continue;
             }
         };
-        let text = match document.extract_text(&[1]) {
-            Ok(val) => val,
-            Err(err) => {
-                eprintln!("Failed to extract text from document: {err}");
-                continue;
-            }
-        };
 
-        let conc = v.concodance(text.to_lowercase());
-        index.insert(doc.clone(), conc);
+        let mut doc_index = HashMap::new();
+        let end = document.get_pages().len() as u32;
+
+        for i in 1..end {
+            let text = match document.extract_text(&[i]) {
+                Ok(val) => val,
+                Err(err) => {
+                    eprintln!("Failed to extract text from document: {err}");
+                    continue;
+                }
+            };
+
+            let tokens = Lexer::new(&text).parse();
+            doc_index = v.concodance(&tokens, doc_index);
+        }
+
+        docs_index.insert(doc.clone(), doc_index);
     }
     println!("Completed Indexing!");
 
     let file = BufWriter::new(File::create("index.json")?);
     println!("Writing into Index.json...");
 
-    serde_json::to_writer(file, &index)?;
+    serde_json::to_writer(file, &docs_index)?;
     Ok(())
 }
 
@@ -142,11 +193,13 @@ fn search_term(v: &VectorCompare, term: &str, index_file: &str) -> io::Result<Ve
     let file = BufReader::new(File::open(index_file)?);
     let docs_index: DocsIndex = serde_json::from_reader(file)?;
 
-    let term_conc = v.concodance(term.to_string());
+    let tokens = Lexer::new(term).parse();
+    let term_conc = v.concodance(&tokens, HashMap::new());
     for (doc_name, doc_index) in docs_index.iter() {
-        let relation = v.relation(&term_conc, doc_index);
+        let mut relation = v.relation(&term_conc, doc_index);
 
         if relation != 0.0 {
+            relation = relation.log10().abs();
             matches.push((relation, doc_name.clone()));
         }
     }
@@ -160,55 +213,56 @@ fn entry() -> io::Result<Commands> {
     let mut args = env::args();
 
     if args.len() < 2 {
+        if let Some(program) = args.next() {
+            usage(&program);
+        };
+
         return Err(io::Error::new(
             io::ErrorKind::InvalidInput,
             "Missing some Arguments",
         ));
     }
-    args.next().unwrap(); //program
+    let program = args.next().expect("Failed to parse program name");
 
     match args.next().unwrap().as_str() {
         "search" => {
-            let index_file = match args.next() {
-                Some(file) => file,
-                None => {
-                    return Err(io::Error::new(
+            if let Some(index_file) = args.next() {
+                if let Some(term) = args.next() {
+                    Ok(Commands::Search { term, index_file })
+                } else {
+                    Err(io::Error::new(
                         io::ErrorKind::InvalidInput,
-                        "Missing index file",
-                    ));
+                        "Missing Search Term",
+                    ))
                 }
-            };
-
-            let term = match args.next() {
-                Some(t) => t,
-                None => {
-                    return Err(io::Error::new(
-                        io::ErrorKind::InvalidInput,
-                        "Missing index file",
-                    ));
-                }
-            };
-
-            Ok(Commands::Search { term, index_file })
+            } else {
+                Err(io::Error::new(
+                    io::ErrorKind::InvalidInput,
+                    "Missing index file",
+                ))
+            }
         }
         "index" => {
-            let index_file = match args.next() {
-                Some(file) => file,
-                None => {
-                    return Err(io::Error::new(
-                        io::ErrorKind::NotFound,
-                        "Missing index file",
-                    ));
-                }
-            };
+            if let Some(dir) = args.next() {
+                Ok(Commands::Index { files_dir: dir })
+            } else {
+                usage(&program);
 
-            Ok(Commands::Index {
-                files_dir: index_file,
-            })
+                Err(io::Error::new(
+                    io::ErrorKind::InvalidInput,
+                    "Missing Directory",
+                ))
+            }
         }
         _ => Err(io::Error::new(
             io::ErrorKind::InvalidInput,
             "Missing some Arguments",
         )),
     }
+}
+
+fn usage(program: &str) {
+    println!("USAGE: {program}");
+    println!("\tsearch <index> <term>       Search for a term in documents");
+    println!("\tindex <directory>           Create an index from a directory");
 }
