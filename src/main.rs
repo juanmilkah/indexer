69d3f0d11fae0132b4f973263f8708f1e77344c2
Juan Milkah <juanmilkah@gmail.com>
2025-01-28 13:54:26 +0300

    index, search

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
 use std::collections::HashMap;
+use std::fs::{self, File};
+use std::{
+    env,
+    io::{self, BufReader, BufWriter},
+};
 
-struct VectorCompare {}
+use lopdf::Document;
+
+enum Commands {
+    Search { term: String, index_file: String },
+    Index { files_dir: String },
+}
+
+type DocsIndex = HashMap<String, DocIndex>;
+type DocIndex = HashMap<String, f32>;
+
+struct VectorCompare;
 
 impl VectorCompare {
     // count of every word that occurs in a document
@@ -17,7 +32,7 @@ impl VectorCompare {
         map
     }
 
-    fn magnitude(&self, concodance: &HashMap<String, f32>) -> f32 {
+    fn magnitude(&self, concodance: &DocIndex) -> f32 {
         let mut total = 0.0;
 
         for (_, count) in concodance.iter() {
@@ -27,11 +42,7 @@ impl VectorCompare {
         total.sqrt()
     }
 
-    fn relation(
-        &self,
-        concodance_1: &HashMap<String, f32>,
-        concodance_2: &HashMap<String, f32>,
-    ) -> f32 {
+    fn relation(&self, concodance_1: &DocIndex, concodance_2: &DocIndex) -> f32 {
         let mut top_value: f32 = 0.0;
 
         for (word, count) in concodance_1.iter() {
@@ -51,67 +62,153 @@ impl VectorCompare {
     }
 }
 
-fn main() {
-    let documents = HashMap::from([
-        (
-            0,
-            "At Scale You Will Hit Every Performance Issue I used to think I knew a bit about performance scalability and how to keep things trucking when you hit large amounts of data Truth is I know diddly squat on the subject since the most I have ever done is read about how its done To understand how I came about realising this you need some background".to_string(),
-        ),
-        (
-            1,
-            "Richard Stallman to visit Australia Im not usually one to promote events and the like unless I feel there is a genuine benefit to be had by attending but this is one stands out Richard M Stallman the guru of Free Software is coming Down Under to hold a talk You can read about him here Open Source Celebrity to visit Australia".to_string(),
+fn main() -> io::Result<()> {
+    let v = VectorCompare;
+    match entry() {
+        Ok(val) => match val {
+            Commands::Index { files_dir } => {
+                let docs = fs::read_dir(&files_dir)
+                    .unwrap()
+                    .map(|entry| entry.unwrap().path().to_string_lossy().to_string())
+                    .collect::<Vec<String>>();
 
-        ),
-        (
-            2,
-            "MySQL Backups Done Easily One thing that comes up a lot on sites like Stackoverflow and the like is how to backup MySQL databases The first answer is usually use mysqldump This is all fine and good till you start to want to dump multiple databases You can do this all in one like using the all databases option however this makes restoring a single database an issue since you have to parse out the parts you want which can be a pain".to_string(),
-        ),
-        (
-            3,
-            "Why You Shouldnt roll your own CAPTCHA At a TechEd I attended a few years ago I was watching a presentation about Security presented by Rocky Heckman read his blog its quite good In it he was talking about security algorithms The part that really stuck with me went like this".to_string(),
-        ),
-        (
-            4,
-            "The Great Benefit of Test Driven Development Nobody Talks About The feeling of productivity because you are writing lots of code Think about that for a moment Ask any developer who wants to develop why they became a developer One of the first things that comes up is I enjoy writing code This is one of the things that I personally enjoy doing Writing code any code especially when its solving my current problem makes me feel productive It makes me feel like Im getting somewhere Its empowering".to_string(),
-        ),
-        (
-            5,
-            "Setting up GIT to use a Subversion SVN style workflow Moving from Subversion SVN to GIT can be a little confusing at first I think the biggest thing I noticed was that GIT doesnt have a specific workflow you have to pick your own Personally I wanted to stick to my Subversion like work-flow with a central server which all my machines would pull and push too Since it took a while to set up I thought I would throw up a blog post on how to do it".to_string(),
-        ),
-        (
-            6,
-            "Why CAPTCHA Never Use Numbers 0 1 5 7 Interestingly this sort of question pops up a lot in my referring search term stats Why CAPTCHAs never use the numbers 0 1 5 7 Its a relativity simple question with a reasonably simple answer Its because each of the above numbers are easy to confuse with a letter See the below".to_string(),
-        ),
-    ]);
+                if index_documents(&v, &docs).is_err() {
+                    return Err(io::Error::new(
+                        io::ErrorKind::InvalidData,
+                        "Failed to build Index",
+                    ));
+                }
+            }
+            Commands::Search { term, index_file } => {
+                let term_matches = match search_term(&v, &term, &index_file) {
+                    Ok(val) => val,
+                    Err(err) => {
+                        return Err(err);
+                    }
+                };
 
-    let v = VectorCompare {};
+                if term_matches.is_empty() {
+                    println!("No Matches!");
+                    return Ok(());
+                }
 
+                for m in term_matches.iter().take(10) {
+                    println!("{}: \t{}", m.0, m.1);
+                }
+            }
+        },
+        Err(err) => return Err(err),
+    };
+
+    Ok(())
+}
+
+fn index_documents(v: &VectorCompare, docs: &[String]) -> io::Result<()> {
     //build the index
     let mut index = HashMap::new();
 
-    for (key, value) in documents.iter() {
-        let conc = v.concodance(value.to_lowercase());
-        index.insert(key, conc);
-    }
+    for doc in docs.iter() {
+        println!("Indexing document: {doc}");
+        let document = match Document::load(doc) {
+            Ok(doc) => doc,
+            Err(err) => {
+                eprintln!("Failed to load document: {err}");
+                continue;
+            }
+        };
+        let text = match document.extract_text(&[1]) {
+            Ok(val) => val,
+            Err(err) => {
+                eprintln!("Failed to extract text from document: {err}");
+                continue;
+            }
+        };
 
-    let search_term = "benefit security databases".to_string();
+        let conc = v.concodance(text.to_lowercase());
+        index.insert(doc.clone(), conc);
+    }
+    println!("Completed Indexing!");
+
+    let file = BufWriter::new(File::create("index.json")?);
+    println!("Writing into Index.json...");
+
+    serde_json::to_writer(file, &index)?;
+    Ok(())
+}
+
+fn search_term(v: &VectorCompare, term: &str, index_file: &str) -> io::Result<Vec<(f32, String)>> {
     let mut matches = Vec::new();
 
-    for i in 0..index.len() {
-        let relation = v.relation(
-            &v.concodance(search_term.to_lowercase()),
-            index.get(&i).unwrap(),
-        );
+    let file = BufReader::new(File::open(index_file)?);
+    let docs_index: DocsIndex = serde_json::from_reader(file)?;
+
+    let term_conc = v.concodance(term.to_string());
+    for (doc_name, doc_index) in docs_index.iter() {
+        let relation = v.relation(&term_conc, doc_index);
 
         if relation != 0.0 {
-            let doc = documents.get(&i).unwrap();
-            matches.push((relation, doc));
+            matches.push((relation, doc_name.clone()));
         }
     }
-    matches.sort_by_key(|k| k.0 as u32);
-    matches.reverse();
 
-    for m in matches {
-        println!("{}:  {}\n\n", m.0, m.1);
+    matches.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
+    matches.reverse();
+    Ok(matches)
+}
+
+fn entry() -> io::Result<Commands> {
+    let mut args = env::args();
+
+    if args.len() < 2 {
+        return Err(io::Error::new(
+            io::ErrorKind::InvalidInput,
+            "Missing some Arguments",
+        ));
+    }
+    args.next().unwrap(); //program
+
+    match args.next().unwrap().as_str() {
+        "search" => {
+            let index_file = match args.next() {
+                Some(file) => file,
+                None => {
+                    return Err(io::Error::new(
+                        io::ErrorKind::InvalidInput,
+                        "Missing index file",
+                    ));
+                }
+            };
+
+            let term = match args.next() {
+                Some(t) => t,
+                None => {
+                    return Err(io::Error::new(
+                        io::ErrorKind::InvalidInput,
+                        "Missing index file",
+                    ));
+                }
+            };
+
+            Ok(Commands::Search { term, index_file })
+        }
+        "index" => {
+            let index_file = match args.next() {
+                Some(file) => file,
+                None => {
+                    return Err(io::Error::new(
+                        io::ErrorKind::NotFound,
+                        "Missing index file",
+                    ));
+                }
+            };
+
+            Ok(Commands::Index {
+                files_dir: index_file,
+            })
+        }
+        _ => Err(io::Error::new(
+            io::ErrorKind::InvalidInput,
+            "Missing some Arguments",
+        )),
     }
 }
