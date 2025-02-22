fd1005128173597c224cafee4482604921bce0d0
Juan Milkah <juanmilkah@gmail.com>
2025-02-22 09:54:31 +0300

    index documents in parallel

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use std::fs::{self, File};
 use std::io::{self, BufReader, BufWriter};
 use std::path::{Path, PathBuf};
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::{Arc, Mutex};
 use std::time::SystemTime;
 use std::{char, env};
+
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 
 mod lexer;
 mod models;
@@ -176,23 +180,20 @@ fn get_index_table(filepath: &str) -> io::Result<models::IndexTable> {
 }
 
 fn doc_index_is_expired(doc: &str, index_table: &models::IndexTable) -> Option<bool> {
-    match index_table.tables.get(doc) {
-        Some(doc_table) => {
-            let now = SystemTime::now();
-            let modified_at = Path::new(&doc).metadata().unwrap().modified().unwrap();
-            let elapsed_since_modified = now.duration_since(modified_at).unwrap();
-            let elapsed_since_indexed = now.duration_since(doc_table.indexed_at).unwrap();
+    if let Some(doc_table) = index_table.tables.get(doc) {
+        let now = SystemTime::now();
+        let modified_at = Path::new(&doc).metadata().unwrap().modified().unwrap();
+        let elapsed_since_modified = now.duration_since(modified_at).unwrap();
+        let elapsed_since_indexed = now.duration_since(doc_table.indexed_at).unwrap();
 
-            Some(elapsed_since_indexed > elapsed_since_modified)
-        }
-        None => None,
-    }
+        return Some(elapsed_since_indexed > elapsed_since_modified);
+    };
+    None
 }
 
 fn index_doc_by_extension(model: &mut models::Model, doc: &str) -> io::Result<DocHandler> {
     let doc_extension = Path::new(&doc).extension();
     match doc_extension {
-        // maybe we should try some threading here
         Some(ext) => match ext.to_str().unwrap() {
             "pdf" => match parsers::index_pdf_document(model, doc) {
                 Ok(()) => Ok(DocHandler::Indexed),
@@ -241,46 +242,57 @@ fn main() -> io::Result<()> {
             } => {
                 let files_dir = PathBuf::from(files_dir);
                 let docs = read_files_recursively(&files_dir)?;
-                let index_table = match get_index_table(&index_path) {
-                    Ok(val) => val,
-                    Err(_) => models::IndexTable::new(),
-                };
-                let mut model = models::Model::new(index_table);
-                let mut indexed_docs = 0;
-                let mut skipped = 0;
+                let index_table =
+                    get_index_table(&index_path).unwrap_or_else(|_| models::IndexTable::new());
 
-                'classify: for doc in docs {
+                // process the documents in parallel
+                let model = Arc::new(Mutex::new(models::Model::new(index_table)));
+                let indexed_docs = Arc::new(AtomicUsize::new(0));
+                let skipped = Arc::new(AtomicUsize::new(0));
+
+                docs.par_iter().for_each(|doc| {
                     // check if document index exists in the index_table;
                     // if it exixts, check whether the file has been modified
                     // since the last index
                     // if yes then reindex the file
                     // if no then skip the file
-                    if let Some(is_expired) = doc_index_is_expired(&doc, &model.index_table) {
+                    if let Some(is_expired) =
+                        doc_index_is_expired(doc, &model.lock().unwrap().index_table)
+                    {
                         if !is_expired {
                             println!("Skipped {doc}");
-                            skipped += 1;
-                            continue 'classify;
+                            skipped.fetch_add(1, Ordering::Relaxed);
+                            return;
                         }
                     }
 
                     //match the document's file extension and index it accordingly
-                    match index_doc_by_extension(&mut model, &doc) {
-                        Ok(DocHandler::Skipped) => skipped += 1,
-                        Ok(DocHandler::Indexed) => indexed_docs += 1,
-                        Err(e) => eprintln!("{e}"),
-                    }
-                }
-
+                    match index_doc_by_extension(&mut model.lock().unwrap(), doc) {
+                        Ok(DocHandler::Skipped) => skipped.fetch_add(1, Ordering::Relaxed),
+                        Ok(DocHandler::Indexed) => indexed_docs.fetch_add(1, Ordering::Relaxed),
+                        Err(e) => {
+                            eprintln!("{e}");
+                            0
+                        }
+                    };
+                });
+                //
                 // update the models idf
-                model.update_idf();
+                model.lock().unwrap().update_idf();
 
                 // write the documents index_table in the provided file path
 
-                println!("Indexed {indexed_docs} documents!");
-                println!("Skipped {skipped} documents!");
+                println!(
+                    "Indexed {indexed_docs} documents!",
+                    indexed_docs = indexed_docs.load(Ordering::Relaxed)
+                );
+                println!(
+                    "Skipped {skipped} documents!",
+                    skipped = skipped.load(Ordering::Relaxed)
+                );
                 println!("Writing into {index_path}...");
                 let file = BufWriter::new(File::create(index_path)?);
-                serde_json::to_writer(file, &model.index_table)?;
+                serde_json::to_writer(file, &model.lock().unwrap().index_table)?;
             }
             Commands::Query { index_file, term } => {
                 let term_matches = match search_term(&term, &index_file) {
